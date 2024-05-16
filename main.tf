4129f2bf89b7c55b1c7d6ebfc8f0ed33af004409
borisyanev88 <borisyanev88@gmail.com>
2024-05-16 17:47:56 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
 }
-
+#asdasd
 resource "aws_s3_bucket_website_configuration" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
 
